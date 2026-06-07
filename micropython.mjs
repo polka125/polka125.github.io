b8b462b624888b9d04a2c74e0a7afa9de9f45768
Sergei NOVOZHILOV <snovozhilov@connect.ust.hk>
2026-06-07 22:27:31 +0800

    fixing some async issues, i don't know, ask gemini

--- micropython.mjs
+++ micropython.mjs
@@ -4335,7 +4335,7 @@ var findStringEnd = (heapOrArray, idx, maxBytesToRead, ignoreNul) => {
   Disabled:3,
   },
   state:0,
-  StackSize:4096,
+  StackSize:32768,
   currData:null,
   handleSleepReturnValue:0,
   exportCallStack:[],
@@ -5871,17 +5871,40 @@ export async function loadMicroPython(options) {
             Module._free(buf);
             return proxy_convert_mp_to_js_obj_jsside_with_free(value);
         },
-        runPythonAsync(code) {
+        // runPythonAsync(code) {
+        //     const len = Module.lengthBytesUTF8(code);
+        //     const buf = Module._malloc(len + 1);
+        //     Module.stringToUTF8(code, buf, len + 1);
+        //     const value = Module._malloc(3 * 4);
+        //     Module.ccall(
+        //         "mp_js_do_exec_async",
+        //         "number",
+        //         ["pointer", "number", "pointer"],
+        //         [buf, len, value],
+        //     );
+        //     Module._free(buf);
+        //     const ret = proxy_convert_mp_to_js_obj_jsside_with_free(value);
+        //     if (ret instanceof PyProxyThenable) {
+        //         return Promise.resolve(ret);
+        //     }
+        //     return ret;
+        // },
+
+        async runPythonAsync(code) {
             const len = Module.lengthBytesUTF8(code);
             const buf = Module._malloc(len + 1);
             Module.stringToUTF8(code, buf, len + 1);
             const value = Module._malloc(3 * 4);
-            Module.ccall(
+            
+            // Add await and the { async: true } flag
+            await Module.ccall(
                 "mp_js_do_exec_async",
                 "number",
                 ["pointer", "number", "pointer"],
                 [buf, len, value],
+                { async: true }
             );
+            
             Module._free(buf);
             const ret = proxy_convert_mp_to_js_obj_jsside_with_free(value);
             if (ret instanceof PyProxyThenable) {
@@ -5889,6 +5912,7 @@ export async function loadMicroPython(options) {
             }
             return ret;
         },
+
         replInit() {
             Module.ccall("mp_js_repl_init", "null", ["null"]);
         },
